182dd439c288d8a4898706766217900034c00b1d
MisterRK <robert.keller22@gmail.com>
2020-09-24 22:17:41 -0400

    Got the basics of the rotating welcome-section working. Animation next.

--- index.js
+++ index.js
@@ -11,3 +11,24 @@ function closeNav() {
 function animateHamburger(){
    document.getElementById("hamburger").classList.toggle("change")
 }
+
+function animateHeadLine(){
+   let counter = 0;
+   let choices = ["Full Stack Developer", "Front End Developer", "Agile Developer"]
+   setInterval(()=>{
+      if(counter > 2){
+         counter = 0
+      }
+      let headline = document.getElementById("headline")
+      headline.innerText = choices[counter]
+      // let oldTag = document.querySelectorAll(".headline")[counter-1]
+      // let newTag = document.querySelectorAll(".headline")[counter]
+      // oldTag === null || oldTag === undefined ? null : oldTag.classList = "headline is-hidden"
+      // newTag.classList = "headline is-visible"
+      counter++;
+   }, 3000)
+}
+
+window.addEventListener("DOMContentLoaded", ()=> {
+   animateHeadLine()
+})
