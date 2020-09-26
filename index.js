d338c0f3c83252ca3a7c3e15d914a26b4ad18d49
MisterRK <robert.keller22@gmail.com>
2020-09-26 00:05:23 -0400

    Removing animation working for headline.

--- index.js
+++ index.js
@@ -12,23 +12,42 @@ function animateHamburger(){
    document.getElementById("hamburger").classList.toggle("change")
 }
 
-function animateHeadLine(){
+function changeHeadLine(){
    let counter = 0;
-   let choices = ["Full Stack Developer", "Front End Developer", "Agile Developer"]
-   setInterval(()=>{
+   let choices = ["Full Stack Developer", "Agile Developer", "Front End Developer"]
+   let animation = setInterval(()=>{
       if(counter > 2){
          counter = 0
       }
       let headline = document.getElementById("headline")
+      removeHeadline();
       headline.innerText = choices[counter]
-      // let oldTag = document.querySelectorAll(".headline")[counter-1]
-      // let newTag = document.querySelectorAll(".headline")[counter]
-      // oldTag === null || oldTag === undefined ? null : oldTag.classList = "headline is-hidden"
-      // newTag.classList = "headline is-visible"
+      // addHeadline()
       counter++;
    }, 3000)
 }
 
+function addHeadline(string){ // the string is the new headline we want to display
+   let headlineText = document.getElementById("headline").innerText // empty string
+   let counter = 0
+   console.log(headlineText)
+}
+
+function removeHeadline(nextString){
+   // console.log("🔫")
+   let headlineText = document.getElementById("headline").innerText // str of headline text
+   // console.log("rat")
+   let removing = setInterval((id) => {
+      // console.log("tat")
+      if(headlineText.length !== 0){
+         headlineText = headlineText.substring(0, headlineText.length -1)
+         document.getElementById("headline").innerText = headlineText
+      } else {
+         clearInterval(removing);
+      }
+   }, 100);
+}
+
 window.addEventListener("DOMContentLoaded", ()=> {
-   animateHeadLine()
+   changeHeadLine()
 })
